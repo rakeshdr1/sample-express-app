209fce0045620b34874c7b08a219a227f714cd17
Rakesh <rakesh@rapidinnovation.dev>
2023-04-11 06:44:20 +0530

    updated port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(5001, () => {
+  console.log("Example app listening on port 5001!");
 });
